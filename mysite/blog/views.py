c6f666b59fa8535341db73c5ca86cc66306a2c9c
GHLisz <leeshzh@outlook.com>
2017-06-28 22:23:05 +0800

    Adding URL patterns for your views

---
+++ mysite/blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .models import Post
+
+
+# Create your views here.
+def post_list(request, category=None):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
