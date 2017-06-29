ddba9a9e2bf55c138b6ef1843326ec291bf25cc3
GHLisz <leeshzh@outlook.com>
2017-06-29 22:48:38 +0800

    chapter 1 end

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,9 +5,25 @@ from .models import Post
 
 
 # Create your views here.
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
 def post_list(request, category=None):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
