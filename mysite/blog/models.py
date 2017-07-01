361acc655cc0d4451ddc3f20e4f6de65eacd0ad9
GHLisz <leeshzh@outlook.com>
2017-07-01 22:04:14 +0800

    chapter 2 end

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 
 # Create your models here.
@@ -26,6 +27,7 @@ class Post(models.Model):
 
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # The Dahl-specific manager.
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
