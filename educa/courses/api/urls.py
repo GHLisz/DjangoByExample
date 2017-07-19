ece50f73298cb3c45220eb65bf42cb00e4224b1b
GHLisz <leeshzh@outlook.com>
2017-07-19 22:55:03 +0800

    c12

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -3,13 +3,13 @@ from rest_framework import routers
 from . import views
 
 
-# router = routers.DefaultRouter()
-# router.register('courses', views.CourseViewSet)
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
 
 
 urlpatterns = [
     url(r'^subjects/$', views.SubjectListView.as_view(), name='subject_list'),
     url(r'^subjects/(?P<pk>\d+)/$', views.SubjectDetailView.as_view(), name='subject_detail'),
-    url(r'^courses/(?P<pk>\d+)/enroll/$', views.CourseEnrollView.as_view(), name='course_enroll'),
-    # url(r'^', include(router.urls)),
+    # url(r'^courses/(?P<pk>\d+)/enroll/$', views.CourseEnrollView.as_view(), name='course_enroll'),
+    url(r'^', include(router.urls)),
 ]
